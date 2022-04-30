e9e67d0ab2c9133cba9eafe6d6cca287c8061c70
Md-Istiaq <mdistiaq102@outlook.com>
2022-05-01 00:41:27 +0600

    api for single product created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion ,ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 const app = express()
 
@@ -23,6 +23,12 @@ async function run(){
             res.send(products)
         })
 
+        app.get('/product/:id', async(req,res) =>{
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const product = await productCollection.findOne(query)
+            res.send(product)
+        })
 
     }
     finally{
