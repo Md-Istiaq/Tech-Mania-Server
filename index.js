0b0d33bba981ffc3f84c7c8d735e5bd45e59f39c
Md-Istiaq <mdistiaq102@outlook.com>
2022-05-01 00:07:07 +0600

    root api created

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://user2:V8f93SloD9YTCaIy@cluster0.4yyma.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const productCollection = client.db('Inventory').collection('product')
+
+        app.get('/product',async (req,res) =>{
+            const query={}
+            const cursor = productCollection.find(query)
+            const products = await cursor.toArray()
+            res.send(products)
+        })
+
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir)
 
 app.get('/' , (req,res) =>{
     res.send("Running genius server")
